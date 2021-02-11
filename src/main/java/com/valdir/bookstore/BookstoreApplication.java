72c6af18107c8af5fa991f6d36ef7fd9b2868200
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-11 16:41:25 -0200

    perfil de teste implementado

--- src/main/java/com/valdir/bookstore/BookstoreApplication.java
+++ src/main/java/com/valdir/bookstore/BookstoreApplication.java
@@ -1,39 +1,13 @@
 package com.valdir.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.valdir.bookstore.domain.Categoria;
-import com.valdir.bookstore.domain.Livro;
-import com.valdir.bookstore.repositories.CategoriaRepository;
-import com.valdir.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }
---
+++ src/main/java/com/valdir/bookstore/service/DBService.java
@@ -0,0 +1,40 @@
+package com.valdir.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdir.bookstore.domain.Categoria;
+import com.valdir.bookstore.domain.Livro;
+import com.valdir.bookstore.repositories.CategoriaRepository;
+import com.valdir.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public void instanciaBaseDeDados() {
+		
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de informatica");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
+
+		cat1.getLivros().addAll(Arrays.asList(l1, l2));
+		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	}
+
+}
