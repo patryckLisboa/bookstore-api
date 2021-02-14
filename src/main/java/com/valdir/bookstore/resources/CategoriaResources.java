2c223b83eaa62bc0d2369629d51b5a36d7fd617e
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-13 23:08:51 -0200

    post para categoria

--- src/main/java/com/valdir/bookstore/resources/CategoriaResources.java
+++ src/main/java/com/valdir/bookstore/resources/CategoriaResources.java
@@ -1,5 +1,6 @@
 package com.valdir.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.valdir.bookstore.domain.Categoria;
 import com.valdir.bookstore.dtos.CategoriaDTO;
@@ -35,4 +39,12 @@ public class CategoriaResources {
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@PostMapping	
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = categoriaSerice.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/valdir/bookstore/service/CategoriaService.java
+++ src/main/java/com/valdir/bookstore/service/CategoriaService.java
@@ -26,4 +26,10 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
 	}
+	
+	public Categoria create(Categoria obj) {
+		obj.setId(null);
+		
+		return categoriaRepository.save(obj); 
+	}
 }
