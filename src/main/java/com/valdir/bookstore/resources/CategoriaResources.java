0770041e8faacdd599fa907c628e62420b9a6417
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-14 18:21:44 -0200

    exceção personalizada para delete de categoria

--- src/main/java/com/valdir/bookstore/resources/CategoriaResources.java
+++ src/main/java/com/valdir/bookstore/resources/CategoriaResources.java
@@ -2,7 +2,7 @@ package com.valdir.bookstore.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
+
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/valdir/bookstore/service/CategoriaService.java
+++ src/main/java/com/valdir/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.valdir.bookstore.domain.Categoria;
@@ -18,25 +19,25 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! id: " + id + ", tipo:" + Categoria.class.getName()));
-		
+
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
-		
-		return categoriaRepository.save(obj); 
+
+		return categoriaRepository.save(obj);
 	}
 
 	public Categoria update(Integer id, CategoriaDTO objDTO) {
 		Categoria obj = findById(id);
-		
+
 		obj.setNome(objDTO.getNome());
 		obj.setDescricao(objDTO.getDescricao());
 		return categoriaRepository.save(obj);
@@ -44,6 +45,13 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		categoriaRepository.deleteById(id);
+
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) { // do spring
+			throw new com.valdir.bookstore.service.DataIntegrityViolationException(
+					"Categoria não pode ser deletado. Pois possui livros associados");
+		}
+
 	}
 }
