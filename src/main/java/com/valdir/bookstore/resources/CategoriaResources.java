46b7ad57addc407a592542e73dae666fc46add6a
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-13 23:38:38 -0200

    delete para categoria

--- src/main/java/com/valdir/bookstore/resources/CategoriaResources.java
+++ src/main/java/com/valdir/bookstore/resources/CategoriaResources.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,12 @@ public class CategoriaResources {
 		Categoria newObj = categoriaSerice.update(id, objDTO);
 		
 		return ResponseEntity.ok(new CategoriaDTO(newObj));
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		categoriaSerice.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/valdir/bookstore/service/CategoriaService.java
+++ src/main/java/com/valdir/bookstore/service/CategoriaService.java
@@ -41,4 +41,9 @@ public class CategoriaService {
 		obj.setDescricao(objDTO.getDescricao());
 		return categoriaRepository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		findById(id);
+		categoriaRepository.deleteById(id);
+	}
 }
