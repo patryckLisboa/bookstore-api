90caa3a00cbc48799d61e5716e8af90295fa2fef
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-15 11:09:29 -0200

    put e patch para livro

--- src/main/java/com/valdir/bookstore/resources/LisvroResources.java
+++ src/main/java/com/valdir/bookstore/resources/LisvroResources.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +41,19 @@ public class LisvroResources {
 		return ResponseEntity.ok().body(listDTO);
 		
 		//localhost:8080/livros?categorias=1
+	}
+	
+	@PutMapping(value = "/{id}") // atualizar informação total
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){ //variavel e corpo da requisção, resposta
+		Livro newObj = livroService.update(id, obj);
+		
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PatchMapping(value = "/{id}") // atualizar informação parcial
+	public ResponseEntity<Livro> updatePath(@PathVariable Integer id, @RequestBody Livro obj){ //variavel e corpo da requisção, resposta
+		Livro newObj = livroService.update(id, obj);
+		
+		return ResponseEntity.ok().body(newObj);
 	}
 }
--- src/main/java/com/valdir/bookstore/service/LivroService.java
+++ src/main/java/com/valdir/bookstore/service/LivroService.java
@@ -32,5 +32,19 @@ public class LivroService {
 		
 		return livroRepository.findAllByCategoria(id_cat);
 	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		
+		return livroRepository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+		
+	}
 	
 }
