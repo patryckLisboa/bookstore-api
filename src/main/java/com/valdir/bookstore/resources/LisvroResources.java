c468bbcdd731135d8e0eae906595531c8a3c138c
patryckLisboa <patryck-lisboa@hotmail.com>
2021-02-15 11:37:56 -0200

    Post para livro

--- src/main/java/com/valdir/bookstore/resources/LisvroResources.java
+++ src/main/java/com/valdir/bookstore/resources/LisvroResources.java
@@ -1,5 +1,6 @@
 package com.valdir.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.valdir.bookstore.domain.Livro;
 import com.valdir.bookstore.dtos.LivroDTO;
@@ -30,6 +33,8 @@ public class LisvroResources {
 		Livro obj = livroService.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+		//http://localhost:8080/livros/{codigo do livro a ser encontrado}
+	
 	}
 	
 	@GetMapping
@@ -48,12 +53,26 @@ public class LisvroResources {
 		Livro newObj = livroService.update(id, obj);
 		
 		return ResponseEntity.ok().body(newObj);
+		
+		//http://localhost:8080/livros/id e passando um livro modificado
 	}
 	
 	@PatchMapping(value = "/{id}") // atualizar informação parcial
 	public ResponseEntity<Livro> updatePath(@PathVariable Integer id, @RequestBody Livro obj){ //variavel e corpo da requisção, resposta
 		Livro newObj = livroService.update(id, obj);
 		
+		
 		return ResponseEntity.ok().body(newObj);
+		//http://localhost:8080/livros/id e passando um livro modificado
+	}
+	
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria" , defaultValue = "0") Integer id_cat,
+			@RequestBody Livro obj){ //defaul - se não passar valor, o valor é zero
+		
+		Livro newObj = livroService.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();//uri é o que vai editar do cabecalho da requisição
+		return ResponseEntity.created(uri).build(); // vai passar a url onde se encontra o novo livro salvo
+		//http://localhost:8080/livros?categoria=3  e passando um livro a ser incluido
 	}
 }
--- src/main/java/com/valdir/bookstore/service/LivroService.java
+++ src/main/java/com/valdir/bookstore/service/LivroService.java
@@ -6,37 +6,37 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.valdir.bookstore.domain.Categoria;
 import com.valdir.bookstore.domain.Livro;
 import com.valdir.bookstore.repositories.LivroRepository;
 import com.valdir.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class LivroService {
-	
-	@Autowired //o spring que vai gerenciar esse instancia/ hora que vai gerar e destrouir
+
+	@Autowired // o spring que vai gerenciar esse instancia/ hora que vai gerar e destrouir
 	private LivroRepository livroRepository;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
-	
+
 	public Livro findById(Integer id) {
-		Optional<Livro> obj = livroRepository.findById(id); //optional é pq ele pode receber nulo
-	
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado, id:" + id + ", tipo:"+ Livro.class.getName()));
+		Optional<Livro> obj = livroRepository.findById(id); // optional é pq ele pode receber nulo
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado, id:" + id + ", tipo:" + Livro.class.getName()));
 	}
 
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
-		
-		
+
 		return livroRepository.findAllByCategoria(id_cat);
 	}
 
 	public Livro update(Integer id, Livro obj) {
 		Livro newObj = findById(id);
 		updateData(newObj, obj);
-		
+
 		return livroRepository.save(newObj);
 	}
 
@@ -44,7 +44,15 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
-		
+
 	}
-	
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		
+		obj.setCategoria(cat);
+		return livroRepository.save(obj);
+	}
+
 }
